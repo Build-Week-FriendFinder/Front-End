a7b072d431f8f96845a2bb3534f0429841ab8a21
John Wells <molekewl2017@gmail.com>
2019-09-26 12:03:25 -0700

    user populates with data from api

--- friend-finder/src/components/find-friends/User.js
+++ friend-finder/src/components/find-friends/User.js
@@ -1,24 +1,24 @@
 import React from 'react';
 
-const User = ({ profile, name, gender, age, location, interests, next, request }) => {
+const User = ({ name, gender, dob, location, bio, next, request }) => {
   return (
     <div className='user-container'>
       <div className='container'>
         <div className='image-container'>
-          Image
+          <img src={`https://robohash.org/${dob}?set=set4&size=180x180`} alt='user-profile' />
         </div>
         <div className='button-container'>
           <button onClick={request}>Request</button>
           <button onClick={next}>Next</button>
         </div>
         <div className='user-info-container'>
-          <h2>Name: {name}</h2>
-          <h2>gender: {gender}</h2>
-          <h2>age: {age}</h2>
+          <h2>{name}</h2>
+          <h2>{gender}</h2>
+          <h2>birthday: {dob}</h2>
           <h2>location: {location}</h2>
         </div>
         <div className='interests-container'>
-          <h2>interests: {interests}</h2>
+          <h2>Bio: {bio}</h2>
         </div>
       </div>
     </div>
